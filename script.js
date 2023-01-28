016806b9f9ef68ec4e4c4a803cc792f9b366f1da
Deekshya Karki <99348458+dishy2001@users.noreply.github.com>
2023-01-28 20:09:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 function threeSum(arr, target) {
   //your code here
+	const array = arr.sort();
+	let diff = Math.abs(target - 0);
+	let ans = 0
+	for(let i = 0 ; i < array.length ;i++){
+		let j = i+1;
+		let t = target - array[i]+array[j];
+		let s = j+1;
+		let e = array.length;
+		while(s < e){
+			let mid = e - (e-s)/2;
+			if(array[e] <= t){
+				d = Math.abs(t-e);
+				if(d < diff) {
+					diff = d
+					ans = array[i]+array[j]+array[e];
+				};
+				s = mid+1;
+			}
+			else if(array[e] >= t){
+				d = Math.abs(t-e);
+				if(d < diff) {
+					diff = d
+					ans = array[i]+array[j]+array[e];
+				};
+				e = mid-1;
+			}
+			else if(array[e] === t){
+				ans = array[i]+array[j]+array[e];
+				break;
+			}
+		}
+	}
 }
 
 module.exports = threeSum;
